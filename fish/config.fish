b36290535a171700ddcbc6b48d6069b5ef2e5c86
Osman Pontes <osmanpontes@gmail.com>
2017-08-09 09:57:27 -0300

    dsds

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
+if test -e "$HOME/.dotfiles/fish/private.fish";
+    source ~/.config/fish/private.fish
+end
+
 alias vim="nvim"
 
 # Use neovim as default editor
@@ -6,4 +10,7 @@ set -xg EDITOR $VISUAL
 
 # Show hidden files in 'ag', except .git
 set -xg FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
+
+# awscli and others
+set -gx PATH ~/.local/bin $PATH
 
