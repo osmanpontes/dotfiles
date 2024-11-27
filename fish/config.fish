0ca383ea21464d52a7c78349bf871cb8bb68bbe6
Osman Pontes <osmanpontes@gmail.com>
2024-11-27 12:12:26 -0300

    feat(fish): update

--- fish/config.fish
+++ fish/config.fish
@@ -9,7 +9,12 @@ set -xg VISUAL nvim
 set -xg EDITOR $VISUAL
 
 # Show hidden files in 'ag', except .git
-set -xg FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
+# set -xg FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
+set -xg FZF_DEFAULT_COMMAND 'rg --files --hidden -g !.git/'
+
+# This is needed to make bobthefish theme work well
+# https://github.com/oh-my-fish/theme-bobthefish
+set -g theme_nerd_fonts yes
 
 # awscli and others
 # set -gx PATH ~/.local/bin $PATH
